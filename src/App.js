27204b86492a8d57cd9e5763bce09d749d3d6207
minuxx <minux8905@gmail.com>
2023-02-21 04:18:14 +0900

    done #2.3 Animation and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import styled from 'styled-components'
 
 const Father = styled.div`
   display: flex;
+  flex-direction: column;
 `
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
 `
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   )
 }
