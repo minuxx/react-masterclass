4a4d617b092f8c05d4a9c7e523c7b5f966eb68d2
minuxx <minux8905@gmail.com>
2023-02-23 11:54:24 +0900

    done #5.8 Nested Routes part Two

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
-import { Route, Switch, useLocation, useParams } from 'react-router-dom'
+import {
+  Link,
+  Route,
+  Switch,
+  useLocation,
+  useParams,
+  useRouteMatch,
+} from 'react-router-dom'
 import styled from 'styled-components'
 import Chart from './Char'
 import Price from './Price'
-
-interface RouterParams {
-  coinId: string
-}
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -54,6 +57,32 @@ const OverviewItem = styled.div`
 const Description = styled.p`
   margin: 20px 0px;
 `
+
+const Tabs = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 25px 0px;
+  gap: 10px;
+`
+
+const Tab = styled.span<{ isActive: boolean }>`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: 400;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 7px 0px;
+  border-radius: 10px;
+  color: ${(props) =>
+    props.isActive ? props.theme.accentColor : props.theme.textColor};
+  a {
+    display: block;
+  }
+`
+
+interface RouterParams {
+  coinId: string
+}
 
 interface RouteState {
   name: string
@@ -121,6 +150,8 @@ function Coin() {
   const { state } = useLocation<RouteState>()
   const [info, setInfo] = useState<IInfoData>()
   const [priceInfo, setPriceInfo] = useState<IPriceData>()
+  const chartMatch = useRouteMatch('/:coinId/chart')
+  const priceMatch = useRouteMatch('/:coinId/price')
 
   useEffect(() => {
     ;(async () => {
@@ -172,6 +203,16 @@ function Coin() {
               <span>{priceInfo?.max_supply}</span>
             </OverviewItem>
           </Overview>
+
+          <Tabs>
+            <Tab isActive={chartMatch !== null}>
+              <Link to={`/${coinId}/chart`}>Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !== null}>
+              <Link to={`/${coinId}/price`}>Price</Link>
+            </Tab>
+          </Tabs>
+
           <Switch>
             <Route path={`/${coinId}/price`}>
               <Price />
