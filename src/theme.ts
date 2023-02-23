10da9667e86cc503584b66d97e23be86ec9047cd
minuxx <minux8905@gmail.com>
2023-02-23 11:29:13 +0900

    done #5.7 Nested Routes part One

--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,5 @@ import { DefaultTheme } from 'styled-components'
 export const theme: DefaultTheme = {
   bgColor: '#2c3e50',
   textColor: '#3498db',
-  accentColor: '#e74c3c',
+  accentColor: '#074cec',
 }
