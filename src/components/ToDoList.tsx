7d5cf18316edcd667a4aa418fe6d6d1cd4831373
minuxx <minux8905@gmail.com>
2023-03-01 22:08:32 +0900

    done #6.17 Selectors part Two

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,40 +1,37 @@
-import { useRecoilValue } from 'recoil'
-import { toDoSelector, toDoState } from '../atoms'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { categoryState, toDoSelector } from '../atoms'
 import CreateToDo from './CreateToDo'
 import ToDo from './ToDo'
 
 function ToDoList() {
-  const [toDo, doing, done] = useRecoilValue(toDoSelector) // 반환된 세 개의 배열을 각각 받는다.
+  const toDos = useRecoilValue(toDoSelector) // 반환된 세 개의 배열을 각각 받는다.
+  const [category, setCategory] = useRecoilState(categoryState)
+
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    const {
+      currentTarget: { value },
+    } = event
+    setCategory(value)
+  }
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
 
+      <form>
+        <select value={category} onInput={onInput}>
+          <option value="TO_DO">To Do</option>
+          <option value="DOING">Doing</option>
+          <option value="DONE">Done</option>
+        </select>
+      </form>
+
       <CreateToDo />
-      <h2>To Do</h2>
-      <ul>
-        {toDo.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
 
-      <h2>Doing</h2>
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-
-      <h2>Done</h2>
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   )
 }
