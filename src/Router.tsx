bd029abe1048c19703a3ca85da7f8aad8f4cd52c
minuxx <minux8905@gmail.com>
2023-03-03 01:26:02 +0900

    reset project

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,19 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Coin from './routes/Coin'
-import Coins from './routes/Coins'
-import Price from './routes/Price'
-import Chart from './routes/Chart'
 
 function Router() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Coins />} />
-        <Route path="/:coinId/" element={<Coin />}>
-          <Route path="price" element={<Price />} />
-          <Route path="chart" element={<Chart />} />
-        </Route>
-      </Routes>
+      <Routes></Routes>
     </BrowserRouter>
   )
 }
