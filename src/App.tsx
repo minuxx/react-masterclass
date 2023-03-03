2ca1123d5c277f7d69287e3e07a3d89641586b01
minuxx <minux8905@gmail.com>
2023-03-03 10:12:29 +0900

    done #8.7 MotionValues part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import { motion } from 'framer-motion'
-import { useRef } from 'react'
+import { motion, useMotionValue, useMotionValueEvent } from 'framer-motion'
+import { useEffect, useRef } from 'react'
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -29,27 +29,17 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: '100px' },
-}
-
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null)
+  const x = useMotionValue(0)
+
+  useMotionValueEvent(x, 'change', (changedX) => {
+    console.log(changedX)
+  })
 
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.3}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+      <button onClick={() => x.set(200)}></button>
+      <Box style={{ x }} drag="x" dragSnapToOrigin />
     </Wrapper>
   )
 }
