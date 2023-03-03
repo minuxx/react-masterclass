efb0181b747c222819f3a45ea2f97ebfd3c3de9b
minuxx <minux8905@gmail.com>
2023-03-03 15:04:22 +0900

    done #8.12 Slider part One

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const Box = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
+  font-size: 32px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
