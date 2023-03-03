e8de137f41e46e22a6be2898a2270da1d82083f9
minuxx <minux8905@gmail.com>
2023-03-03 16:00:16 +0900

    done #8.14 You Need to Watch This

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,50 @@
 import styled from 'styled-components'
-import { motion, AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 import { useState } from 'react'
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 32px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
-const boxVariants = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
-}
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
 
 function App() {
-  const [visible, setVisible] = useState(1)
-  const [back, setBack] = useState(false)
-  const nextPlease = async () => {
-    await setBack(false)
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1))
-  }
-  const prevPlease = async () => {
-    await setBack(true)
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1))
-  }
+  const [clicked, setClicked] = useState(false)
+  const toggleClicked = () => setClicked((prev) => !prev)
 
   return (
-    <Wrapper>
-      <AnimatePresence>
-        <Box
-          custom={back}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <div>
-        <button onClick={prevPlease}>prev</button>
-        <button onClick={nextPlease}>next</button>
-      </div>
+    <Wrapper onClick={toggleClicked}>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: '50px' }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: '0px', scale: 2 }} />
+        ) : null}
+      </Box>
     </Wrapper>
   )
 }
