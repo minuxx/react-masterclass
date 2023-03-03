39bc71dadd8c6264dea33dcd156364be63175839
minuxx <minux8905@gmail.com>
2023-03-03 13:52:12 +0900

    done #8.9 MotionValues part Three

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,15 @@ import {
   useMotionValue,
   useMotionValueEvent,
   useTransform,
+  useScroll,
 } from 'framer-motion'
-import { useEffect, useRef } from 'react'
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled(motion.div)`
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
-`
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
 `
 
 const Box = styled(motion.div)`
@@ -34,24 +23,28 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
 
-// transformation
-// -800 => 2
-// -400 => 1.5
-// 0    => 1
-// 400  => 0.5
-// 800  => 0
-
 function App() {
   const x = useMotionValue(0)
-  const scale = useTransform(x, [-800, 0, 800], [2, 1, 0]) // motionValue, input, output
+  const rotate = useTransform(x, [-800, 0, 800], [-360, 1, 360]) // motionValue, input, output
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      'linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))',
+      'linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))',
+    ]
+  )
 
-  useMotionValueEvent(scale, 'change', (s) => {
-    console.log(s)
+  const { scrollY, scrollYProgress } = useScroll()
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5])
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    console.log(latest)
   })
 
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, rotate, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   )
 }
