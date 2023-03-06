ebcc45fcaa18a56a928dd8d8a885d3b7d301c392
minuxx <minux8905@gmail.com>
2023-03-07 00:42:42 +0900

    done #9.6 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = 'ed8f5e6c3b01b09d1cf884f7315fc7de'
 const BASE_URL = 'https://api.themoviedb.org/3'
 
+interface IMovie {
+  id: number
+  backdrop_path: string
+  poster_path: string
+  title: string
+  overview: string
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string
+    minimun: string
+  }
+  page: number
+  results: IMovie[]
+  total_page: number
+  total_results: number
+}
+
 export function getMovies() {
   return fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
--- src/index.tsx
+++ src/index.tsx
@@ -62,9 +62,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-weight: 300;
     font-family: 'Source Sans Pro', sans-serif;
-    color:black;
     line-height: 1.2;
-    
+    color: ${(props) => props.theme.white.darker};
+    background-color: black;
   }
   a {
     text-decoration:none;
